5349d9d0dee0026d88b34f584f4b40f0a5e9534a
Maciej Kosiarski <maciej.kosiarski@performance-media.pl>
2019-02-11 12:36:35 +0100

    not defined logger channel exception message modified

--- Service/LoggerFactory.php
+++ Service/LoggerFactory.php
@@ -21,7 +21,8 @@ class LoggerFactory
     public function getLogger(string $channel): LoggerInterface
     {
         if (!array_key_exists($channel, $this->channels)) {
-            throw new \InvalidArgumentException('You are trying to reach not defined logger channel');
+        	$format = 'You are trying to reach not defined logger channel "%s", (defined: %s)';
+            throw new \InvalidArgumentException(sprintf($format, $channel, implode(', ', $this->channels)));
         }
 
         return $this->channels[$channel];
